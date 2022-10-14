d5dc400265443f6825ed1719e102c1c13eb95b56
Romain Xu-Darme <romain.xu@univ-grenoble-alpes.fr>
2022-10-14 17:01:34 +0200

    main_tree.py training process can now be resumed using checkpoints.

--- main_tree.py
+++ main_tree.py
@@ -1,7 +1,4 @@
-from prototree.prototree import ProtoTree
-from util.log import Log
-
-from util.args import get_args, save_args, get_optimizer
+from util.args import get_args, save_args, get_optimizer, load_args
 from util.data import get_dataloaders
 from util.init import init_tree
 from util.net import get_network, freeze
@@ -15,16 +12,25 @@ from prototree.project import project, project_with_class_constraints
 from prototree.upsample import upsample
 
 import torch
-from shutil import copy
 from copy import deepcopy
 
 def run_tree(args=None):
     args = args or get_args()
+
+    resume = False
+    if (os.path.exists(args.log_dir) and os.path.exists(args.log_dir+'/metadata')
+            and load_args(args.log_dir+'/metadata') == args and os.path.exists(args.log_dir+'/checkpoints/latest')) \
+            or args.state_dict_dir_tree != '':
+        # Directory already exists and contains the same arguments => resume computation
+        # Alternatively, if state_dict_dir_tree is specified, restart from a given checkpoint
+        resume = True
+
     # Create a logger
-    log = Log(args.log_dir)
+    log = Log(args.log_dir, mode='a' if resume else 'w')
     print("Log dir: ", args.log_dir, flush=True)
     # Create a csv log for storing the test accuracy, mean train accuracy and mean loss for each epoch
-    log.create_log('log_epoch_overview', 'epoch', 'test_acc', 'mean_train_acc', 'mean_train_crossentropy_loss_during_epoch')
+    log.create_log('log_epoch_overview', 'epoch', 'test_acc', 'mean_train_acc',
+                   'mean_train_crossentropy_loss_during_epoch')
     # Log the run arguments
     save_args(args, log.metadata_dir)
     if not args.disable_cuda and torch.cuda.is_available():
@@ -43,78 +49,107 @@ def run_tree(args=None):
 
     # Obtain the dataset and dataloaders
     trainloader, projectloader, testloader, classes, num_channels = get_dataloaders(args)
-    # Create a convolutional network based on arguments and add 1x1 conv layer
-    features_net, add_on_layers = get_network(num_channels, args)
-    # Create a ProtoTree
-    tree = ProtoTree(num_classes=len(classes),
-                    feature_net = features_net,
-                    args = args,
-                    add_on_layers = add_on_layers)
-    tree = tree.to(device=device)
-    # Determine which optimizer should be used to update the tree parameters
-    optimizer, params_to_freeze, params_to_train = get_optimizer(tree, args)
-    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer=optimizer, milestones=args.milestones, gamma=args.gamma)
-    tree, epoch = init_tree(tree, optimizer, scheduler, device, args)
 
-    tree.save(f'{log.checkpoint_dir}/tree_init')
-    log.log_message("Max depth %s, so %s internal nodes and %s leaves"%(args.depth, tree.num_branches, tree.num_leaves))
-    analyse_output_shape(tree, trainloader, log, device)
+    if not resume:
+        # Create a convolutional network based on arguments and add 1x1 conv layer
+        features_net, add_on_layers = get_network(num_channels, args)
+        # Create a ProtoTree
+        tree = ProtoTree(num_classes=len(classes),
+                         feature_net=features_net,
+                         args=args,
+                         add_on_layers=add_on_layers)
+        tree = tree.to(device=device)
+        # Determine which optimizer should be used to update the tree parameters
+        optimizer, params_to_freeze, params_to_train = get_optimizer(tree, args)
+        scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer=optimizer, milestones=args.milestones,
+                                                         gamma=args.gamma)
+        tree, epoch = init_tree(tree, optimizer, scheduler, device, args)
+        log.log_message(
+            "Max depth %s, so %s internal nodes and %s leaves" % (args.depth, tree.num_branches, tree.num_leaves))
+        analyse_output_shape(tree, trainloader, log, device)
 
-    leaf_labels = dict()
-    best_train_acc = 0.
-    best_test_acc = 0.
+        leaf_labels = dict()
+        best_train_acc = 0.
+        best_test_acc = 0.
 
-    if epoch < args.epochs + 1:
+        save_checkpoint(
+            f'{log.checkpoint_dir}/tree_init', tree, optimizer, scheduler, 0,
+            best_train_acc, best_test_acc, leaf_labels, args)
+
+    else:
+        # Either latest checkpoint or the one pointed by state_dict_dir_tree
+        directory_path = log.checkpoint_dir+'/latest' if not args.state_dict_dir_tree else args.state_dict_dir_tree
+        print('Resuming computation from ' + directory_path)
+        tree, (optimizer, params_to_freeze, params_to_train), scheduler, stats = \
+            load_checkpoint(directory_path)
+        best_train_acc, best_test_acc, leaf_labels, epoch = stats
+        # Go to the next epoch
+        epoch += 1
+
+    if epoch < args.epochs+1:
         '''
             TRAIN AND EVALUATE TREE
         '''
         for epoch in range(epoch, args.epochs + 1):
-            log.log_message("\nEpoch %s"%str(epoch))
+            log.log_message("\nEpoch %s" % str(epoch))
             # Freeze (part of) network for some epochs if indicated in args
             freeze(tree, epoch, params_to_freeze, params_to_train, args, log)
             log_learning_rates(optimizer, args, log)
 
             # Train tree
             if tree._kontschieder_train:
-                train_info = train_epoch_kontschieder(tree, trainloader, optimizer, epoch, args.disable_derivative_free_leaf_optim, device, log, log_prefix)
+                train_info = train_epoch_kontschieder(
+                    tree, trainloader, optimizer, epoch,
+                    args.disable_derivative_free_leaf_optim, device, log, log_prefix)
             else:
-                train_info = train_epoch(tree, trainloader, optimizer, epoch, args.disable_derivative_free_leaf_optim, device, log, log_prefix)
-            save_tree(tree, optimizer, scheduler, epoch, log, args)
-            best_train_acc = save_best_train_tree(tree, optimizer, scheduler, best_train_acc, train_info['train_accuracy'], log)
+                train_info = train_epoch(
+                    tree, trainloader, optimizer, epoch,
+                    args.disable_derivative_free_leaf_optim, device, log, log_prefix)
+            # Update scheduler and leaf labels before saving checkpoints
+            scheduler.step()
             leaf_labels = analyse_leafs(tree, epoch, len(classes), leaf_labels, args.pruning_threshold_leaves, log)
 
+            # Update best train accuracy (if necessary)
+            best_train_acc = save_best_train_tree(
+                tree, optimizer, scheduler, epoch,
+                train_info['train_accuracy'], best_train_acc, best_test_acc, leaf_labels, args, log)
+            save_tree(
+                tree, optimizer, scheduler, epoch,
+                best_train_acc, best_test_acc, leaf_labels, args, log)
+
             # Evaluate tree
-            if args.epochs>100:
-                if epoch%10==0 or epoch==args.epochs:
-                    eval_info = eval(tree, testloader, epoch, device, log)
-                    original_test_acc = eval_info['test_accuracy']
-                    best_test_acc = save_best_test_tree(tree, optimizer, scheduler, best_test_acc, eval_info['test_accuracy'], log)
-                    log.log_values('log_epoch_overview', epoch, eval_info['test_accuracy'], train_info['train_accuracy'], train_info['loss'])
-                else:
-                    log.log_values('log_epoch_overview', epoch, "n.a.", train_info['train_accuracy'], train_info['loss'])
-            else:
+            if args.epochs <= 100 or epoch % 10 == 0 or epoch == args.epochs:
                 eval_info = eval(tree, testloader, epoch, device, log)
                 original_test_acc = eval_info['test_accuracy']
-                best_test_acc = save_best_test_tree(tree, optimizer, scheduler, best_test_acc, eval_info['test_accuracy'], log)
-                log.log_values('log_epoch_overview', epoch, eval_info['test_accuracy'], train_info['train_accuracy'], train_info['loss'])
-
-            scheduler.step()
+                best_test_acc = save_best_test_tree(
+                    tree, optimizer, scheduler, epoch,
+                    best_train_acc, original_test_acc, best_test_acc, leaf_labels, args, log)
+                log.log_values('log_epoch_overview', epoch,
+                               original_test_acc, train_info['train_accuracy'], train_info['loss'])
+            else:
+                log.log_values('log_epoch_overview', epoch, "n.a.",
+                               train_info['train_accuracy'], train_info['loss'])
 
     else: #tree was loaded and not trained, so evaluate only
         '''
             EVALUATE TREE
         '''
+        # Readjust epoch index
+        epoch = args.epochs
         eval_info = eval(tree, testloader, epoch, device, log)
         original_test_acc = eval_info['test_accuracy']
-        best_test_acc = save_best_test_tree(tree, optimizer, scheduler, best_test_acc, eval_info['test_accuracy'], log)
-        log.log_values('log_epoch_overview', epoch, eval_info['test_accuracy'], "n.a.", "n.a.")
+        best_test_acc = save_best_test_tree(
+            tree, optimizer, scheduler, epoch,
+            best_train_acc, original_test_acc, best_test_acc, leaf_labels, args, log)
+        log.log_values('log_epoch_overview', epoch, original_test_acc, "n.a.", "n.a.")
 
     '''
         EVALUATE AND ANALYSE TRAINED TREE
     '''
     log.log_message("Training Finished. Best training accuracy was %s, best test accuracy was %s\n"%(str(best_train_acc), str(best_test_acc)))
-    # Save tree (again)
-    save_tree_description(tree, optimizer, scheduler, "trained", log)
+    # Save tree (for sanity checks)
+    save_checkpoint(f'{log.checkpoint_dir}/trained',
+                    tree, optimizer, scheduler, epoch, best_train_acc, best_test_acc, leaf_labels, args)
 
     trained_tree = deepcopy(tree)
     leaf_labels = analyse_leafs(tree, epoch+1, len(classes), leaf_labels, args.pruning_threshold_leaves, log)
@@ -123,9 +158,10 @@ def run_tree(args=None):
     '''
         PRUNE
     '''
-    pruned = prune(tree, args.pruning_threshold_leaves, log)
+    prune(tree, args.pruning_threshold_leaves, log)
     name = "pruned"
-    save_tree_description(tree, optimizer, scheduler, name, log)
+    save_checkpoint(f'{log.checkpoint_dir}/{name}',
+                    tree, optimizer, scheduler, epoch, best_train_acc, best_test_acc, leaf_labels, args)
     pruned_tree = deepcopy(tree)
     # Analyse and evaluate pruned tree
     leaf_labels = analyse_leafs(tree, epoch+2, len(classes), leaf_labels, args.pruning_threshold_leaves, log)
@@ -133,14 +169,13 @@ def run_tree(args=None):
     eval_info = eval(tree, testloader, name, device, log)
     pruned_test_acc = eval_info['test_accuracy']
 
-    pruned_tree = tree
-
     '''
         PROJECT
     '''
-    project_info, tree = project_with_class_constraints(deepcopy(pruned_tree), projectloader, device, args, log)
+    project_info, tree = project_with_class_constraints(tree, projectloader, device, args, log)
     name = "pruned_and_projected"
-    save_tree_description(tree, optimizer, scheduler, name, log)
+    save_checkpoint(f'{log.checkpoint_dir}/{name}',
+                    tree, optimizer, scheduler, epoch, best_train_acc, best_test_acc, leaf_labels, args)
     pruned_projected_tree = deepcopy(tree)
     # Analyse and evaluate pruned tree with projected prototypes
     average_distance_nearest_image(project_info, tree, log)
@@ -159,8 +194,9 @@ def run_tree(args=None):
     # visualize tree
     gen_vis(tree, name, args, classes)
 
-
-    return trained_tree.to('cpu'), pruned_tree.to('cpu'), pruned_projected_tree.to('cpu'), original_test_acc, pruned_test_acc, pruned_projected_test_acc, project_info, eval_info_samplemax, eval_info_greedy, fidelity_info
+    return trained_tree.to('cpu'), pruned_tree.to('cpu'), pruned_projected_tree.to('cpu'), \
+        original_test_acc, pruned_test_acc, pruned_projected_test_acc, \
+        project_info, eval_info_samplemax, eval_info_greedy, fidelity_info
 
 
 if __name__ == '__main__':
--- util/save.py
+++ util/save.py
@@ -1,50 +1,144 @@
 import torch
 import os
 import argparse
+import pickle
 from prototree.prototree import ProtoTree
 from util.log import Log
+from util.args import save_args, load_args, get_optimizer
+from typing import Tuple, Any, List
 
-def save_tree(tree: ProtoTree, optimizer, scheduler, epoch: int, log: Log, args: argparse.Namespace):
+
+def save_checkpoint(
+        directory_path: str,
+        tree: ProtoTree,
+        optimizer,
+        scheduler,
+        epoch: int,
+        best_train_acc: float,
+        best_test_acc: float,
+        leaf_labels: dict,
+        args: argparse.Namespace):
+    """ Save everything needed to restart a training process
+
+    :param directory_path: Target location
+    :param tree: ProtoTree
+    :param optimizer: Optimizer
+    :param scheduler: Scheduler
+    :param epoch: Current epoch
+    :param best_train_acc: Current best training accuracy
+    :param best_test_acc: Current best test accuracy
+    :param leaf_labels: Current leaf labels
+    :param args: Command line arguments
+    """
+    if not os.path.isdir(directory_path):
+        os.mkdir(directory_path)
     tree.eval()
+    tree.save(directory_path)
+    tree.save_state(directory_path)
+    torch.save(optimizer.state_dict(), f'{directory_path}/optimizer_state.pth')
+    torch.save(scheduler.state_dict(), f'{directory_path}/scheduler_state.pth')
+    # Save random state
+    torch_rng_state = torch.random.get_rng_state()
+    stats = {'best_train_acc': best_train_acc,
+             'best_test_acc': best_test_acc,
+             'leaf_labels': leaf_labels,
+             'epoch': epoch,
+             'torch_random_state': torch_rng_state}
+    with open(f'{directory_path}/stats.pickle', 'wb') as f:
+        pickle.dump(stats, f)
+    save_args(args, directory_path)
+
+
+def load_checkpoint(directory_path: str) -> \
+        Tuple[ProtoTree, Tuple[Any, List, List], Any, Tuple[float, float, dict, int]]:
+    """ Restore training process using checkpoint directory.
+
+    :param directory_path: Target location
+    :returns: tree, (optimizer, params_to_freeze, params_to_train), scheduler,
+        (best_train_acc, best_test_acc, leaf_labels, epoch)
+    """
+    if not os.path.isdir(directory_path):
+        raise ValueError(f"Unknown checkpoint directory {directory_path}")
+
+    # Load all arguments
+    args = load_args(directory_path)
+    if not args.disable_cuda and torch.cuda.is_available():
+        device = torch.device('cuda:{}'.format(torch.cuda.current_device()))
+    else:
+        device = torch.device('cpu')
+
+    # Load tree, optimizer, scheduler
+    tree = ProtoTree.load(directory_path, map_location=str(device))
+    optimizer, params_to_freeze, params_to_train = get_optimizer(tree, args)
+    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer=optimizer, milestones=args.milestones, gamma=args.gamma)
+    optimizer.load_state_dict(torch.load(f'{directory_path}/optimizer_state.pth', map_location=str(device)))
+    scheduler.load_state_dict(torch.load(f'{directory_path}/scheduler_state.pth', map_location=str(device)))
+
+    # Recover current training stats
+    with open(directory_path + '/stats.pickle', 'rb') as f:
+        stats = pickle.load(f)
+    best_train_acc, best_test_acc, leaf_labels, epoch, rng_state = tuple(stats.values())
+    # Resume torch random state
+    torch.random.set_rng_state(rng_state)
+    print(f"Checkpoint loaded. Epoch: {epoch}, best train acc: {best_train_acc}, best test acc: {best_test_acc}")
+    return tree, (optimizer, params_to_freeze, params_to_train), scheduler, \
+           (best_train_acc, best_test_acc, leaf_labels, epoch)
+
+
+def save_tree(
+        tree: ProtoTree,
+        optimizer,
+        scheduler,
+        epoch: int,
+        best_train_acc: float,
+        best_test_acc: float,
+        leaf_labels: dict,
+        args: argparse.Namespace,
+        log: Log,
+):
     # Save latest model
-    tree.save(f'{log.checkpoint_dir}/latest')
-    tree.save_state(f'{log.checkpoint_dir}/latest')
-    torch.save(optimizer.state_dict(), f'{log.checkpoint_dir}/latest/optimizer_state.pth')
-    torch.save(scheduler.state_dict(), f'{log.checkpoint_dir}/latest/scheduler_state.pth')
+    save_checkpoint(f'{log.checkpoint_dir}/latest',
+                    tree, optimizer, scheduler, epoch, best_train_acc, best_test_acc, leaf_labels, args)
 
     # Save model every 10 epochs
-    if epoch == args.epochs or epoch%10==0:
-        tree.save(f'{log.checkpoint_dir}/epoch_{epoch}')
-        tree.save_state(f'{log.checkpoint_dir}/epoch_{epoch}')
-        torch.save(optimizer.state_dict(), f'{log.checkpoint_dir}/epoch_{epoch}/optimizer_state.pth')
-        torch.save(scheduler.state_dict(), f'{log.checkpoint_dir}/epoch_{epoch}/scheduler_state.pth')
+    if epoch == args.epochs or epoch % 10 == 0:
+        save_checkpoint(f'{log.checkpoint_dir}/epoch_{epoch}',
+                        tree, optimizer, scheduler, epoch, best_train_acc, best_test_acc, leaf_labels, args)
 
-def save_best_train_tree(tree: ProtoTree, optimizer, scheduler, best_train_acc: float, train_acc: float, log: Log):
-    tree.eval()
+
+def save_best_train_tree(
+        tree: ProtoTree,
+        optimizer,
+        scheduler,
+        epoch: int,
+        train_acc: float,
+        best_train_acc: float,
+        best_test_acc: float,
+        leaf_labels: dict,
+        args: argparse.Namespace,
+        log: Log,
+):
     if train_acc > best_train_acc:
         best_train_acc = train_acc
-        tree.save(f'{log.checkpoint_dir}/best_train_model')
-        tree.save_state(f'{log.checkpoint_dir}/best_train_model')
-        torch.save(optimizer.state_dict(), f'{log.checkpoint_dir}/best_train_model/optimizer_state.pth')
-        torch.save(scheduler.state_dict(), f'{log.checkpoint_dir}/best_train_model/scheduler_state.pth')
+        save_checkpoint(f'{log.checkpoint_dir}/best_train_model',
+                        tree, optimizer, scheduler, epoch, best_train_acc, best_test_acc, leaf_labels, args)
     return best_train_acc
 
-def save_best_test_tree(tree: ProtoTree, optimizer, scheduler, best_test_acc: float, test_acc: float, log: Log):
-    tree.eval()
+
+def save_best_test_tree(
+        tree: ProtoTree,
+        optimizer,
+        scheduler,
+        epoch: int,
+        best_train_acc: float,
+        test_acc: float,
+        best_test_acc: float,
+        leaf_labels: dict,
+        args: argparse.Namespace,
+        log: Log,
+):
     if test_acc > best_test_acc:
         best_test_acc = test_acc
-        tree.save(f'{log.checkpoint_dir}/best_test_model')
-        tree.save_state(f'{log.checkpoint_dir}/best_test_model')
-        torch.save(optimizer.state_dict(), f'{log.checkpoint_dir}/best_test_model/optimizer_state.pth')
-        torch.save(scheduler.state_dict(), f'{log.checkpoint_dir}/best_test_model/scheduler_state.pth')
+        save_checkpoint(f'{log.checkpoint_dir}/best_test_model',
+                        tree, optimizer, scheduler, epoch, best_train_acc, best_test_acc, leaf_labels, args)
     return best_test_acc
-
-def save_tree_description(tree: ProtoTree, optimizer, scheduler, description: str, log: Log):
-    tree.eval()
-    # Save model with description
-    tree.save(f'{log.checkpoint_dir}/'+description)
-    tree.save_state(f'{log.checkpoint_dir}/'+description)
-    if optimizer is not None:
-        torch.save(optimizer.state_dict(), f'{log.checkpoint_dir}/'+description+'/optimizer_state.pth')
-    if scheduler is not None:
-        torch.save(scheduler.state_dict(), f'{log.checkpoint_dir}/'+description+'/scheduler_state.pth')
