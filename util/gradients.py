f9965402bc05008c38419ccd0263a7d35400dd7e
Romain Xu-Darme <romain.xu@univ-grenoble-alpes.fr>
2022-12-14 09:15:28 +0100

    Fix deprecated import.

--- util/gradients.py
+++ util/gradients.py
@@ -1,6 +1,6 @@
 import torch
 import numpy as np
-from scipy.ndimage.filters import gaussian_filter
+from scipy.ndimage import gaussian_filter
 from typing import Optional, Tuple
 
 from prototree.prototree import ProtoTree
